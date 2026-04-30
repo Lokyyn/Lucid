f1b4d4facc1ca260f3a387f8e9cb89015d6c38de
Maximilian <55558686+Lokyyn@users.noreply.github.com>
2026-04-30 16:25:29 +0200

    Replaced LightBlueBorder

--- source/Lucid/Docking/LucidToolWindow.cs
+++ source/Lucid/Docking/LucidToolWindow.cs
@@ -168,7 +168,7 @@ public class LucidToolWindow : LucidDockContent
         // Draw header
         var bgColor = isActive ? ThemeProvider.Theme.Colors.DockActive : ThemeProvider.Theme.Colors.SurfaceDefault;
         var darkColor = isActive ? ThemeProvider.Theme.Colors.BorderAccent : ThemeProvider.Theme.Colors.BorderDefault;
-        var lightColor = isActive ? ThemeProvider.Theme.Colors.LightBlueBorder : ThemeProvider.Theme.Colors.BorderDefault;
+        var lightColor = isActive ? ThemeProvider.Theme.Colors.BorderAccent : ThemeProvider.Theme.Colors.BorderDefault;
 
         using (var b = new SolidBrush(bgColor))
         {
--- source/Lucid/Theming/Themes/CustomTheme.cs
+++ source/Lucid/Theming/Themes/CustomTheme.cs
@@ -56,7 +56,7 @@ internal class CustomTheme : ITheme
         Colors.SurfaceHighlight = Color.FromArgb(92, 92, 92);
         Colors.SurfaceHighlight = Color.FromArgb(82, 82, 82);
         Colors.BorderAccent = Color.FromArgb(51, 61, 78);
-        Colors.LightBlueBorder = Color.FromArgb(86, 97, 114);
+        Colors.BorderAccent = Color.FromArgb(86, 97, 114);
         Colors.SurfaceHighlight = Color.FromArgb(159, 178, 196);   
 
         Sizes.Padding = 10;
