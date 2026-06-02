6fb21906f81f0a9d5ed6b0033bda00773349adfb
Maximilian <55558686+Lokyyn@users.noreply.github.com>
2026-06-02 19:34:44 +0200

    Fix rounded LucidButton invisible until hover due to WS_EX_TRANSPARENT

--- source/Lucid/Controls/LucidButton.cs
+++ source/Lucid/Controls/LucidButton.cs
@@ -501,15 +501,4 @@ public class LucidButton : Button
 
     #endregion
 
-    #region For Transparency
-    protected override CreateParams CreateParams
-    {
-        get
-        {
-            CreateParams cp = base.CreateParams;
-            cp.ExStyle |= 0x20; // WS_EX_TRANSPARENT
-            return cp;
-        }
-    }
-    #endregion
 }
